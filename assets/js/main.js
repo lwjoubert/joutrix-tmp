74aedaeb735eff5a382f56e95d3f4cbb71eae759
Wessel Joubert <143314145+lwjoubert@users.noreply.github.com>
2026-04-24 13:31:52 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 // Reveal-on-scroll (tiny, no libraries)
+document.documentElement.classList.add("js");
 const io = new IntersectionObserver((entries) => {
   entries.forEach(e => {
     if (e.isIntersecting) e.target.classList.add("on");
